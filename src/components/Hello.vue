bd947fa8c6b5fdd6edddda4548a7ff5eb93c5869
thanyadol <rockwalnut@gmail.com>
2020-07-25 03:04:00 +0700

    init app

---
+++ src/components/Hello.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class='hello'>
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  data () {
+    return {
+      msg: 'Hello World with Vue.js 2.5.2 at ' + new Date()
+    }
+  }
+}
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
